e5d21e079c22b2f48dacbd1448b26da6dcc23e6a
its-nandlal <147331160+its-nandlal@users.noreply.github.com>
2025-07-19 15:29:53 +0530

    first commit

--- src/components/home/leandingPage/HeroTop.js
+++ src/components/home/leandingPage/HeroTop.js
@@ -1,5 +1,15 @@
+'use client';
+
 
 export default function HeroTop() {
+
+      const handleScroll = () => {
+    window.scrollTo({
+      top: window.innerWidth < 768 ? 5500 : (600 * window.innerHeight / 100),      
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div
       className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center gap-20 bg-zinc-900/70"
@@ -19,7 +29,7 @@ export default function HeroTop() {
       </div>
 
       <div>
-        <div className="px-6 py-4 bg-zinc-500/30 max-md:-mt-10 max-md:text-[3.5vw] md:text-[1.3vw] font-['f2'] font-[900] uppercaseleading-[1] tracking-wider border border-white backdrop-blur-xs rounded-md cursor-pointer hover:bg-green-500/30 hover:text-zinc-200 ease-in-out duration-200">
+        <div onClick={handleScroll} className="px-6 py-4 bg-zinc-500/30 max-md:-mt-10 max-md:text-[3.5vw] md:text-[1.3vw] font-['f2'] font-[900] uppercaseleading-[1] tracking-wider border border-white backdrop-blur-xs rounded-md cursor-pointer hover:bg-green-500/30 hover:text-zinc-200 ease-in-out duration-200">
           join membership
         </div>
       </div>
