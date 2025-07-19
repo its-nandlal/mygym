6ae6fb788f3186ea6e50c7bbfc43ae9059fd03df
its-nandlal <147331160+its-nandlal@users.noreply.github.com>
2025-07-19 15:41:16 +0530

    first commit

--- src/components/wbbutton/WhatsAppButton.js
+++ src/components/wbbutton/WhatsAppButton.js
@@ -1,13 +1,26 @@
 "use client";
 import React from 'react'
-import { BsWhatsapp } from 'react-icons/bs';
+import { BsWhatsapp } from 'react-icons/bs'
 
-export default function WhatsAppButton() {
+const WhatsAppButton = () => {
+  const phoneNumber = '918209814681'; // Indian country code + provided number
+  const message = encodeURIComponent('Hi, I need help with my new gym website!');
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+
   return (
     <div className='fixed bottom-5 right-5 z-[999]'>
-        <a className='block text-[30px] text-white bg-[#25D366] rounded-full p-4 cursor-pointer hover:bg-[#49a56b] hover:-mt-4 ease-in-out duration-200'>
-          <BsWhatsapp  />
-        </a>
+    <a
+      href={whatsappUrl}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed bottom-4 right-4 z-50 block text-[30px] text-white bg-[#25D366] rounded-full p-4 cursor-pointer hover:bg-[#49a56b] hover:-mt-1 ease-in-out duration-200 shadow-lg"
+      aria-label="Contact us on WhatsApp for help with your gym website"
+    >
+      <BsWhatsapp />
+    </a>
     </div>
-  )
-}
+  );
+};
+
+export default WhatsAppButton;
+
